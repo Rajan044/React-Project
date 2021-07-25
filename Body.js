3f9ff072cab915d6d59473a95dcb74f253d92b85
Rajan Kumar Singh <71922376+Rajan044@users.noreply.github.com>
2021-07-25 16:53:09 +0530

    Add files via upload

---
+++ Body.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import './Header.css'
+export default function Body() {
+    return (
+        <div className="container">
+            <div className="cont1">
+                <img src="https://rewy-gatsby.envytheme.com/static/banner-img1-a4ed4e991a66d374b27fbc545b7288b7.png"></img>
+                <div className="subcont1">
+                    <h1>Secure IT Solutions<br /> Services</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do <br />eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button class="button">Get started</button>
+                </div>
+            </div>
+            <div className="cont2">
+                <span className="tittle">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAYAAAA+stv/AAAFCElEQVRYhb1Xf0yUZRz/vM97dxzecUAiYMiPlSVBLpvm3CDWSHds
+                    QVlgGlNImOVK0tqwRmbTpVuuuSFLmhNTSq22hpoV/RB/wagF00xRkQDxEJUfB3fHy929975Pe9zhzpfj7gW1z3/v8+P7+Tzf7/f5fp+XgwLWzoJoAB8ByAMQC2BAkvCzJ
+                    MkbYx7/ul25/l5xlwBrZ8FMACcAxCntyjIc/3YM5cx//vCJ+ymAKL4P+iO/vZDAmJQUdiA/L3HaAxFg7SyYD2BeoMVankwvXpGyTOm5e4HGZ++TauxET5uSCiAUgKBmPV1fvABAPIDT3LaqG8p53xB41BgURUlQCA9E
+                    vhtAI4DvKNDe8carOYEEsOSiwYy2XLQ2MR0qyGcDKB795oDQaEPoFgAmvwIik6q7JAl7Axl1DHsal68++QcAZzABAKKUA1pCGHmYXwEMPI81Lpdc689a9y2pa/3G+vcA3FTjqdzzjq4et3xXWLvtw0eVofabzWdPvZxrNGqXaLV8jChK1s
+                    rDNKGqTp4zbO1dKp4/9X0wckN67gwW0id0JObLmdqGBD2vs9gd9XP2HqoWKb3qG8JA14nN6ZljSEKKTh8/6zgPLsHZ35MhXmr
+                    8Jxg5QCOd3e3FUsfZBhY9r7dHWE0L6oFxDCcCtDkxebZzQW5BM9HqUm7HU5ZYea7heE3lgY/XRoySuyxthZ7Ocyxf+gLZVS1g1TGLThiy/jglPHKhv3mP6L5xfM8Oqfd6V6hacgZlKQ6EneORM2i0utiMFaunTw03rVNLr
+                    lrAqmOWeb53ejyETDGSjKJ3swAMqT2VqooGYJlagyGmiGyO8A9RWbrpO15R3mAG8BwAO6uMJWvT2ti42hAkqxVACG9KXlwUryCvAsDqywcAtlBKWzZvqnmNzan1gJrKdwd9NmF3WNorrTLHXctOTZMAFPnOcxynDTdFfR4Xl3xMlQB7/60bYVO
+                    jVZG7BLtjxG4fACGphJDMEI3Wr3hCSOSzaS/NvS0gq77sGQBrCMUMSZb/4njyWW361j5Dem4CgE+OV1cuf6HkQ8prNEGvrdPa/4XjzyPbvbHWPGYufIu53d/aq9dah0hWfVk2AHZtCmQOmRxP3pc90plpOUsrAVxmZX2o59rOga62T4ORe1wjfx8
+                    tW7kPQK+3+g1GhEftopSOuZKCYPupsbHGwpJwm7IpEQ0/4+GMSDN1Ow8Jl5oWOZtrNx1atXCDKDjeoVQe8Ufucgz9+svmt4sFy5VO34ZTsjatTxRdmW63k70LZEplwWbr319esY4lZC+XVV/mt7O5Hc7f67K2F3pPc6d55FQciTNGxa6ERjN/sOd6DuV
+                    w2tnTsePE1pLR4jNewrLwhbObCkDy1gqR5QBTnDTGnU53p9fYXY+PH0pe7GZ5oX960VTeYOqThm2nnWd+q/W6PBDYQQeV88z1G8Zskqm1/WDjV97u5Re8wST57ghCPi5IbfrW/W7bSJ4kuOslt+eie1Coubzv5BLLt80XAglQceIJg/e+11hL1anZbEjP
+                    pfq55nIAxsmS+hYi5lLbJGxMpKPev820NP+p6ke1csPccLPlzcXmydqZ1B8OLc1/BMA5FoXRsdZ+2+uz9hzdN1Fbk/VAni85Q4xBv0L55H6QAsbkiihTu/cR+78I2O+Racvoh0Sp7ZsL7ew3LNC19YtJ/+XS0vzQKwO25YIoRe1saqnb1XKVvXD6J2QEwH8PA/Uf4hwXIQAAAABJRU5ErkJggg==" alt="star"></img>
+                    Our solutions
+                </span>
+                <h3>We Different From Others <br />Should Choose Us</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+            </div>
+            <div className="cont3">
+                <nav className="items">
+                    <div>
+                        <img src="https://image.flaticon.com/icons/png/128/1926/1926537.png"></img>
+                        <h3>Code Security</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br />adipiscing elit, sed do eiusmod <br />tempor incididunt ut labore et.</p>
+                        <a className="code" href="#">View Details.</a>
+                    </div>
+                    <div>
+                        <img src="https://image.flaticon.com/icons/png/128/3893/3893055.png"></img>
+                        <h3>Team Management</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br />adipiscing elit, sed do eiusmod <br />tempor incididunt ut labore et.</p>
+                        <a className="team" href="#">View Details.</a>
+                    </div>
+                    <div>
+                        <img src="https://img-premium.flaticon.com/png/128/1422/premium/1422400.png?token=exp=1626969220~hmac=038b8efa0956cd3848399a739ca844bc"></img>
+                        <h3>Access Controlled</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur <br />adipiscing elit, sed do eiusmod <br />tempor incididunt ut labore et.</p>
+                        <a className="access" href="#">View Details.</a>
+                    </div>
+                </nav>
+            </div>
+            <div className="cont4">
+                <img src="https://rewy-gatsby.envytheme.com/static/service1-21e7483cbd4501bdc4672cad93ffa0e0.png"></img>
+                <div className="subcont5">
+                    <div className="sub51">
+                        <span className="title2">
+                            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAiCAYAAAA+stv/AAAFCElEQVRYhb1Xf0yUZRz/vM97dxzecUAiYMiPlSVBLpvm3CDWSHds
+                    QVlgGlNImOVK0tqwRmbTpVuuuSFLmhNTSq22hpoV/RB/wagF00xRkQDxEJUfB3fHy929975Pe9zhzpfj7gW1z3/v8+P7+Tzf7/f5fp+XgwLWzoJoAB8ByAMQC2BAkvCzJ
+                    MkbYx7/ul25/l5xlwBrZ8FMACcAxCntyjIc/3YM5cx//vCJ+ymAKL4P+iO/vZDAmJQUdiA/L3HaAxFg7SyYD2BeoMVankwvXpGyTOm5e4HGZ++TauxET5uSCiAUgKBmPV1fvABAPIDT3LaqG8p53xB41BgURUlQCA9E
+                    vhtAI4DvKNDe8carOYEEsOSiwYy2XLQ2MR0qyGcDKB795oDQaEPoFgAmvwIik6q7JAl7Axl1DHsal68++QcAZzABAKKUA1pCGHmYXwEMPI81Lpdc689a9y2pa/3G+vcA3FTjqdzzjq4et3xXWLvtw0eVofabzWdPvZxrNGqXaLV8jChK1s
+                    rDNKGqTp4zbO1dKp4/9X0wckN67gwW0id0JObLmdqGBD2vs9gd9XP2HqoWKb3qG8JA14nN6ZljSEKKTh8/6zgPLsHZ35MhXmr
+                    8Jxg5QCOd3e3FUsfZBhY9r7dHWE0L6oFxDCcCtDkxebZzQW5BM9HqUm7HU5ZYea7heE3lgY/XRoySuyxthZ7Ocyxf+gLZVS1g1TGLThiy/jglPHKhv3mP6L5xfM8Oqfd6V6hacgZlKQ6EneORM2i0utiMFaunTw03rVNLr
+                    lrAqmOWeb53ejyETDGSjKJ3swAMqT2VqooGYJlagyGmiGyO8A9RWbrpO15R3mAG8BwAO6uMJWvT2ti42hAkqxVACG9KXlwUryCvAsDqywcAtlBKWzZvqnmNzan1gJrKdwd9NmF3WNorrTLHXctOTZMAFPnOcxynDTdFfR4Xl3xMlQB7/60bYVO
+                    jVZG7BLtjxG4fACGphJDMEI3Wr3hCSOSzaS/NvS0gq77sGQBrCMUMSZb/4njyWW361j5Dem4CgE+OV1cuf6HkQ8prNEGvrdPa/4XjzyPbvbHWPGYufIu53d/aq9dah0hWfVk2AHZtCmQOmRxP3pc90plpOUsrAVxmZX2o59rOga62T4ORe1wjfx8
+                    tW7kPQK+3+g1GhEftopSOuZKCYPupsbHGwpJwm7IpEQ0/4+GMSDN1Ow8Jl5oWOZtrNx1atXCDKDjeoVQe8Ufucgz9+svmt4sFy5VO34ZTsjatTxRdmW63k70LZEplwWbr319esY4lZC+XVV/mt7O5Hc7f67K2F3pPc6d55FQciTNGxa6ERjN/sOd6DuV
+                    w2tnTsePE1pLR4jNewrLwhbObCkDy1gqR5QBTnDTGnU53p9fYXY+PH0pe7GZ5oX960VTeYOqThm2nnWd+q/W6PBDYQQeV88z1G8Zskqm1/WDjV97u5Re8wST57ghCPi5IbfrW/W7bSJ4kuOslt+eie1Coubzv5BLLt80XAglQceIJg/e+11hL1anZbEjP
+                    pfq55nIAxsmS+hYi5lLbJGxMpKPev820NP+p6ke1csPccLPlzcXmydqZ1B8OLc1/BMA5FoXRsdZ+2+uz9hzdN1Fbk/VAni85Q4xBv0L55H6QAsbkiihTu/cR+78I2O+Racvoh0Sp7ZsL7ew3LNC19YtJ/+XS0vzQKwO25YIoRe1saqnb1XKVvXD6J2QEwH8PA/Uf4hwXIQAAAABJRU5ErkJggg==" alt="star"></img>
+                            SERVICES
+                        </span>
+                        <h3>Cloud Hosting Services</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    </div>
+                    <div className="checkboxes">
+                        <a className="subcheck">
+                            <label class="check">Cloud Databases
+                                <input type="checkbox" checked="checked" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">File Storage
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">File Backups
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">Email Servers
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                        </a>
+                        <a className="subcheck">
+                            <label class="check">Website Hosting
+                                <input type="checkbox" checked="checked" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">Forex Trading
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">Remote Desktop
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                            <label class="check">Hybrid Cloud
+                                <input type="checkbox" />
+                                <span class="checkmark"></span>
+                            </label>
+                        </a>
+
+                    </div>
+                </div>
+                <h1>Rest content was same as above thats why I didn't do it again.</h1>
+            </div>
+
+        </div>
+    )
+}
